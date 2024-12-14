086fe7e04ef4586c9d2ef0879352943c59c3254c
SenseiEA <nandacansino@gmail.com>
2024-12-14 20:04:12 +0800

    dwdwdw

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,26 +1,38 @@
 import pytest
 from flask import Flask
-app = Flask(__name__, template_folder='templates')
+import sys
+import os
+import logging
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from myapp import app
+
 
 
 # Create a fixture to set up the Flask app for testing
 @pytest.fixture
 def client():
+
     with app.test_client() as client:
         yield client
 
 # Test the index route
 def test_index(client):
+    print("Testing index...")
     # Send a GET request to the '/' route
+    print("Sending GET request to the '/' route...")
     response = client.get('/')
-    
+
     # Check that the status code is 200 (OK)
+    print("Check that the status code is 200 (OK)...")
     assert response.status_code == 200
     
     # Check if the response contains specific content
+    print("Check if the response contains specific content...")
     assert b'Where you are' in response.data
     assert b'Check your coordinates' in response.data
-
+    print("Index test DONE...")
 # Test the calculate_distance route
 def test_calculate_distance(client):
     # Send a POST request with sample data to '/calculate_distance'
